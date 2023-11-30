9e480f533794643fb68f3eba08659e4419dc852f
crushleorey <crushleorey@outlook.com>
2023-11-30 15:02:42 +0800

    修改依赖

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -31,27 +31,28 @@ svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/goto
 svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
 
 # passwall vssr bypass
-svn co https://github.com/kenzok8/trunk/small/chinadns-ng packages/net/chinadns-ng
-svn co https://github.com/kenzok8/trunk/small/dns2socks packages/net/dns2socks
-svn co https://github.com/kenzok8/trunk/small/dns2tcp packages/net/dns2tcp
-svn co https://github.com/kenzok8/trunk/small/microsocks packages/net/microsocks
-svn co https://github.com/kenzok8/trunk/small/tcping packages/net/tcping
-svn co https://github.com/kenzok8/trunk/small/brook packages/net/brook
-svn co https://github.com/kenzok8/trunk/small/hysteria packages/net/hysteria
-svn co https://github.com/kenzok8/trunk/small/naiveproxy packages/net/naiveproxy
-svn co https://github.com/kenzok8/trunk/small/shadowsocks-rust-sslocal packages/net/shadowsocks-rust-sslocal
-svn co https://github.com/kenzok8/trunk/small/shadowsocks-rust-ssserver packages/net/shadowsocks-rust-ssserver
-svn co https://github.com/kenzok8/trunk/small/shadowsocksr-libev-ssr-local packages/net/shadowsocksr-libev-ssr-local
-svn co https://github.com/kenzok8/trunk/small/shadowsocksr-libev-ssr-redir packages/net/shadowsocksr-libev-ssr-redir
-svn co https://github.com/kenzok8/trunk/small/shadowsocksr-libev-ssr-server packages/net/shadowsocksr-libev-ssr-server
-svn co https://github.com/kenzok8/trunk/small/simple-obfs packages/net/simple-obfs
-svn co https://github.com/kenzok8/trunk/small/sing-box packages/net/sing-box
-svn co https://github.com/kenzok8/trunk/small/trojan-go packages/net/trojan-go
-svn co https://github.com/kenzok8/trunk/small/trojan-plus packages/net/trojan-plus
-svn co https://github.com/kenzok8/trunk/small/v2ray-plugin packages/net/v2ray-plugin
-svn co https://github.com/kenzok8/trunk/small/xray-core packages/net/xray-core
-svn co https://github.com/kenzok8/trunk/small/xray-plugin packages/net/xray-plugin
-svn co https://github.com/kenzok8/trunk/small/tuic-client packages/net/tuic-client
+svn co https://github.com/kenzok8/small/trunk/chinadns-ng packages/net/chinadns-ng
+svn co https://github.com/kenzok8/small/trunk/dns2socks packages/net/dns2socks
+svn co https://github.com/kenzok8/small/trunk/dns2tcp packages/net/dns2tcp
+svn co https://github.com/kenzok8/small/trunk/microsocks packages/net/microsocks
+svn co https://github.com/kenzok8/small/trunk/tcping packages/net/tcping
+svn co https://github.com/kenzok8/small/trunk/brook packages/net/brook
+svn co https://github.com/kenzok8/small/trunk/hysteria packages/net/hysteria
+svn co https://github.com/kenzok8/small/trunk/naiveproxy packages/net/naiveproxy
+svn co https://github.com/kenzok8/small/trunk/shadowsocksr-libev packages/net/shadowsocksr-libev
+svn co https://github.com/kenzok8/small/trunk/shadowsocks-rust-sslocal packages/net/shadowsocks-rust-sslocal
+svn co https://github.com/kenzok8/small/trunk/shadowsocks-rust-ssserver packages/net/shadowsocks-rust-ssserver
+svn co https://github.com/kenzok8/small/trunk/shadowsocksr-libev-ssr-local packages/net/shadowsocksr-libev-ssr-local
+svn co https://github.com/kenzok8/small/trunk/shadowsocksr-libev-ssr-redir packages/net/shadowsocksr-libev-ssr-redir
+svn co https://github.com/kenzok8/small/trunk/shadowsocksr-libev-ssr-server packages/net/shadowsocksr-libev-ssr-server
+svn co https://github.com/kenzok8/small/trunk/simple-obfs packages/net/simple-obfs
+svn co https://github.com/kenzok8/small/trunk/sing-box packages/net/sing-box
+svn co https://github.com/kenzok8/small/trunk/trojan-go packages/net/trojan-go
+svn co https://github.com/kenzok8/small/trunk/trojan-plus packages/net/trojan-plus
+svn co https://github.com/kenzok8/small/trunk/v2ray-plugin packages/net/v2ray-plugin
+svn co https://github.com/kenzok8/small/trunk/xray-core packages/net/xray-core
+svn co https://github.com/kenzok8/small/trunk/xray-plugin packages/net/xray-plugin
+svn co https://github.com/kenzok8/small/trunk/tuic-client packages/net/tuic-client
 
 
 
